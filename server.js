230ec7f3c5df665c2c35d31562bda337e3bd2576
Gautamkumarsolanki <42197609+Gautamkumarsolanki@users.noreply.github.com>
2023-05-05 16:34:00 +0530

    added homepage

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ dotenv.config();
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
+app.use('/',(req,res)=>{
+    res.send("Hey !! Welcome to my Api")
+})
 app.use("/api", require("./route/routes.js"));
 const PORT = process.env.PORT || 5000;
 app.listen(3000, () => console.log(`server is running ${PORT}`));
