5a67ddafd62934b727cbc92339c3446fa7d0df9a
Gautamkumarsolanki <42197609+Gautamkumarsolanki@users.noreply.github.com>
2023-06-06 13:47:27 +0530

    get single user functionality added

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors=require('cors');
 dotenv.config();
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-
+app.use(cors({origin:'*'}))
 app.use("/api", require("./route/routes.js"));
 app.use('/',(req,res)=>{
     res.send("Hey !! Welcome to my Api")
